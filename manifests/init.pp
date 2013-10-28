8685f923ca9d76a293c0b0e100c1e28c45d9a6e1
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2013-10-28 23:24:27 +0100

    Adding right packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,12 +16,24 @@ class kolab (
     $version = '3.0',
   ) inherits kolab::params {
 
-#  file { "/etc/apt/apt.conf.d/99auth": #since we can't really add --force-yes to the apt-get install.       
-#    owner     => root,
-#    group     => root,
-#    content   => "APT::Get::AllowUnauthenticated yes;",
-#    mode      => 644;
-#  }
+  firewall::rule { 'kolab-apt-repo':
+    destination => '178.209.35.107', # mirror.kolabsys.com:80
+    protocol    => tcp,
+    port        => 80,
+    direction   => 'output',
+  }
+
+  include iptables
+  Service[ 'iptables' ] -> Exec['aptget_update']
+
+  # Packages are temporarily unsigned
+  file { "/etc/apt/apt.conf.d/99auth": #since we can't really add --force-yes to the apt-get install.       
+    owner     => root,
+    group     => root,
+    content   => "APT::Get::AllowUnauthenticated yes;",
+    mode      => 644,
+    before    => Exec['aptget_update']
+  }
 
 
   #Add repositories for Kolab
@@ -41,14 +53,14 @@ class kolab (
     ensure => absent,
   }
 
-  package { 'kolab':
+  package { ['kolabd', 'kolabadmin', 'kolab-webadmin', 'php-kolab-filter',
+             'php-kolab-freebusy', 'ldap-account-manager', 'kolab-cyrus-imapd',
+             'kolab-cyrus-admin' ]:
     ensure  => installed,
     require => [ Exec['aptget_update'], Package['exim4'], Package['exim4-base'],
                  Package['exim4-config'], Package['exim4-daemon-light']]
   }
 
-
-#  exec { "rm /etc/apt/apt.conf.d/99auth": } #Remove the ugly hack
 
   #exec {'setup-kolab': 
   #  require => Package['kolab-webadmin']
